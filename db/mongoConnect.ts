d25f9ec2e6f43319d53571fb8d477f7d39e19fc7
davidYAO37 <ykdavid11@gmail.com>
2025-08-18 09:31:22 +0000

    base en ligne

--- db/mongoConnect.ts
+++ db/mongoConnect.ts
@@ -17,25 +17,3 @@ export const db = async () => {
     throw new Error("Impossible de se connecter à MongoDB");
   }
 };
-
-
-
-/* import mongoose from "mongoose";
-
-let isConnected = false;
-
-export const db = async () => {
-  if (isConnected) return;
-
-  try {
-    await mongoose.connect(process.env.MONGO_URI as string, {
-      dbName: "bd_esaymed",
-    });
-    isConnected = true;
-    console.log("✅ Connexion MongoDB réussie");
-  } catch (error) {
-    console.error("❌ Erreur connexion MongoDB:", error);
-    throw new Error("Impossible de se connecter à MongoDB");
-  }
-};
- */
